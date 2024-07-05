4dbeae76dc7d585c758500a08d42ecc3dc888b3f
Vansh Nagpal <vanshnagpal@Vanshs-Laptop.local>
2024-07-05 16:03:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -10,23 +10,23 @@ import { useState } from 'react';
 
 
 function App() {
-  const [bag, setBag] = useState([]); 
+  // const [bag, setBag] = useState([]); 
   
-  function HandlerRemove(idd)
-  {
-    const filtered=bag.filter(ele=>ele.id!==idd);
-    console.log(filtered);
-    setBag(filtered);
-  }
-  function HandlerAddtoBag(item)
-    {
-            setBag([...bag, item]);
+  // function HandlerRemove(idd)
+  // {
+  //   const filtered=bag.filter(ele=>ele.id!==idd);
+  //   console.log(filtered);
+  //   setBag(filtered);
+  // }
+  // function HandlerAddtoBag(item)
+  //   {
+  //           setBag([...bag, item]);
 
-            console.log(bag);
-    }
+  //           console.log(bag);
+  //   }
   return (
 <>
- <div className='row'>
+ {/* <div className='row'>
   {
     bag.map(item=>(
       <div className='card'>
@@ -38,10 +38,10 @@ function App() {
 </div>
 <div className='row'>
   <NewCompo  myfunction={HandlerAddtoBag}/>
-</div> 
+</div>  */}
 {/* <Useeffect/> */}
 
-
+<Fetchingusingaxios/>
 
 </>
   );
--- src/fetch/Cards.js
+++ src/fetch/Cards.js
@@ -1,20 +1,20 @@
+import React from 'react';
+import './Card.css';
 
-function Cards({payload})
-{
-  
-    return (
-        <>
-    <div className="card shadow m-3" style={{width:"18rem"}}>
-    <img src={payload.thumbnail} className="card-img-top" alt="..."/>
-    <div className="card-body">
-    <h5 className="card-title">{payload.title}</h5>
-    <p className="card-text">{payload.description}</p>
-    <a href="#" className="btn btn-primary" >add to cart</a>
-    <button className="btn btn-warning">see details</button>
-    </div>
-    </div>
-
-        </>
-    );
+function Cards({ payload }) {
+  return (
+    <>
+      <div className="card shadow m-3" style={{ width: "18rem" }}>
+        <img src={payload.thumbnail} className="card-img-top" alt={payload.title} />
+        <div className="card-body">
+          <h5 className="card-title">{payload.title}</h5>
+          <p className="card-text">{payload.description}</p>
+          <a href="#" className="btn btn-primary">Add to Cart</a>
+          <button className="btn btn-warning">See Details</button>
+        </div>
+      </div>
+    </>
+  );
 }
+
 export default Cards;
